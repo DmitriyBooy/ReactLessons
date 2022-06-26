340010ac7cecf0b35c2895ba7b959378df4027bf
DmitriyBooy <dimkabumiK@gmail.com>
2022-06-26 17:20:36 +0300

    create app

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+    return(
+        <header className='header'>
+            <img alt='asd' src='https://upload.wikimedia.org/wikipedia/commons/4/4e/Playstation_logo_colour.svg'/>
+        </header>
+    )
+}
+
+export default Header
