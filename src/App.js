17dd30fde0d0a7aaf06ef9cc83cb8cb733538c27
dmitriy.b <dimkabumik@gmail.com>
2023-04-09 23:21:06 +0300

    Раскидал будущую структуру

--- src/App.js
+++ src/App.js
@@ -2,31 +2,65 @@ import './App.css'
 
 const App = () => {
   return (
-    <div className="App">
-      <Header/>
-      <Technologies/>
-    </div>)
-}
+    <div className="app-wrapper">
+      <header className='header'>
+        <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/LEGO_logo.svg/2048px-LEGO_logo.svg.png'/>
+      </header>
 
-const Technologies = () => {
-  return (
-    <div>
-      <ul>
-        <li>css</li>
-        <li>html</li>
-        <li>js</li>
-        <li>react</li>
-      </ul>
-    </div>
-  )
-}
+      <nav className='nav'>
+        <div>
+          <a>
+            Profile
+          </a>
+        </div>
+        <div>
+          <a>
+            Messages
+          </a>
+        </div>
+        <div>
+          <a>
+            News
+          </a>
+        </div>
+        <div>
+          <a>
+            Music
+          </a>
+        </div>
+        <div>
+          <a>
+            Settings
+          </a>
+        </div>
+      </nav>
 
-const Header = () => {
-  return (
-    <div>
-      <a href='#s'>Home</a>
-      <a href='#s'>News Feed</a>
-      <a href='#s'>Messages</a>
+      <div className='content'>
+        <div>
+          <img src="https://images.freeimages.com/images/previews/ac9/railway-hdr-1361893.jpg" />
+        </div>
+
+        <div>
+          https://static.fotor.com/app/features/img/aiimage/scenes/a%20realistic%20fox%20in%20the%20lake%20generated%20by%20ai%20image%20creator.png
+          ava + description
+        </div>
+
+        <div>
+          Posts
+
+          <div>
+            new post
+          </div>
+
+          <div>
+            post 1
+          </div>
+
+          <div>
+            post 2
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
