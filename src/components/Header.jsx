5504348f83852c4fa0c9b5c4eee8f187224aff1a
dmitriy.b <dimkabumik@gmail.com>
2023-04-10 22:58:18 +0300

    Добавил модули стилей

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,8 @@
+import s from  './Header.module.css'
+
 const Header = () => {
   return (
-    <header className='header'>
+    <header className={s.header}>
       <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/LEGO_logo.svg/2048px-LEGO_logo.svg.png'/>
     </header>
   )
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,27 +1,27 @@
+import s from './Profile.module.css'
+
 const Profile = () => {
   return (
-    <div className='content'>
+    <div className={s.content}>
       <div>
         <img src="https://images.freeimages.com/images/previews/ac9/railway-hdr-1361893.jpg" />
       </div>
 
       <div>
-        https://static.fotor.com/app/features/img/aiimage/scenes/a%20realistic%20fox%20in%20the%20lake%20generated%20by%20ai%20image%20creator.png
         ava + description
       </div>
 
       <div>
+        new post
+      </div>
+
+      <div className='posts'>
         Posts
-
-        <div>
-          new post
-        </div>
-
-        <div>
+        <div className={s.item}>
           post 1
         </div>
 
-        <div>
+        <div className={s.item}>
           post 2
         </div>
       </div>
