90a6b6a15c6729be471ea58aa1ab4319170672de
dmitriy.b <dimkabumik@gmail.com>
2023-04-13 23:39:49 +0300

    Добавил активный класс

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,27 +5,42 @@ const Navbar = () => {
   return (
     <nav className={s.nav}>
       <div className={s.item}>
-        <NavLink to='/profile'>
+        <NavLink
+          to='/profile'
+          className={ ({ isActive }) => isActive ? s.active : '' }
+        >
           Profile
         </NavLink>
       </div>
       <div className={s.item}>
-        <NavLink to='/dialogs'>
+        <NavLink
+        to='/dialogs'
+        className={ ({ isActive }) => isActive ? s.active : '' }
+        >
           Messages
         </NavLink>
       </div>
       <div className={s.item}>
-        <NavLink to='/news'>
+        <NavLink
+          to='/news'
+          className={ ({ isActive }) => isActive ? s.active : '' }
+        >
           News
         </NavLink>
       </div>
       <div className={s.item}>
-        <NavLink to='/music'>
+        <NavLink
+          to='/music'
+          className={ ({ isActive }) => isActive ? s.active : '' }
+        >
           Music
         </NavLink>
       </div>
       <div className={s.item}>
-        <NavLink to='/settings'>
+        <NavLink
+          to='/settings'
+          className={ ({ isActive }) => isActive ? s.active : '' }
+        >
           Settings
         </NavLink>
       </div>
